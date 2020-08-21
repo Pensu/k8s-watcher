71435669ba15c897fda33f6af8b8aaf941a3bf14
Peeyush Gupta <peeyushgupta91@gmail.com>
2020-08-21 10:41:38 +0530

    Initial code

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/Pensu/k8s-watcher
+
+go 1.14
+
+require k8s.io/client-go v0.18.6 // indirect
---
+++ main.go
@@ -0,0 +1,100 @@
+/*
+Copyright 2016 The Kubernetes Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+// Note: the example only works with the code within the same release/branch.
+package main
+
+import (
+	"context"
+//	"flag"
+	"fmt"
+//	"os"
+//	"path/filepath"
+	"time"
+
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/client-go/kubernetes"
+	"k8s.io/client-go/tools/clientcmd"
+	//
+	// Uncomment to load all auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth"
+	//
+	// Or uncomment to load specific auth plugins
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/azure"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/gcp"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/oidc"
+	// _ "k8s.io/client-go/plugin/pkg/client/auth/openstack"
+)
+
+func main() {
+
+	config, err := clientcmd.BuildConfigFromFlags("", "./config")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// create the clientset
+	clientset, err := kubernetes.NewForConfig(config)
+	if err != nil {
+		panic(err.Error())
+	}
+	for {
+		pods, err := clientset.CoreV1().Pods("").List(context.TODO(), metav1.ListOptions{})
+		if err != nil {
+			panic(err.Error())
+		}
+		fmt.Printf("There are %d pods in the cluster\n", len(pods.Items))
+
+                nodes, err := clientset.CoreV1().Nodes().List(context.TODO(), metav1.ListOptions{})
+                fmt.Println(nodes.Items[0].ObjectMeta.Name)
+                fmt.Println(nodes.Items[0].ObjectMeta.Labels)
+
+
+		for i := 0; i < len(nodes.Items); i++ {
+			node_name := nodes.Items[i].ObjectMeta.Name
+			node_label := nodes.Items[i].ObjectMeta.Labels
+			for label, _ := range node_label {
+				if label == "arch" {
+					fmt.Println("Arch label found in node ", node_name)
+					break
+				}
+			}
+			fmt.Println("No arch label found in node ", node_name)
+		}
+
+
+		// Examples for error handling:
+     		// - Use helper functions like e.g. errors.IsNotFound()
+		// - And/or cast to StatusError and use its properties like e.g. ErrStatus.Message
+		namespace := "default"
+		pod := "example-xxxxx"
+		_, err = clientset.CoreV1().Pods(namespace).Get(context.TODO(), pod, metav1.GetOptions{})
+		if errors.IsNotFound(err) {
+			fmt.Printf("Pod %s in namespace %s not found\n", pod, namespace)
+		} else if statusError, isStatus := err.(*errors.StatusError); isStatus {
+			fmt.Printf("Error getting pod %s in namespace %s: %v\n",
+				pod, namespace, statusError.ErrStatus.Message)
+		} else if err != nil {
+			panic(err.Error())
+		} else {
+			fmt.Printf("Found pod %s in namespace %s\n", pod, namespace)
+		}
+
+		time.Sleep(10 * time.Second)
+	}
+}
